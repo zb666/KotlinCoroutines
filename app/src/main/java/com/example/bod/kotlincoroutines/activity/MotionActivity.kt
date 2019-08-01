b105d96ef84191b0c77bf6a595a7ba9df15b2e05
zb666 <1360799362@qq.com>
2019-08-01 12:31:20 +0800

    motionlayout 过渡动画

---
+++ app/src/main/java/com/example/bod/kotlincoroutines/activity/MotionActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bod.kotlincoroutines.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.bod.kotlincoroutines.R
+
+/**
+ *
+ * @ClassName: MotionActivity
+ * @Description:
+ * @CreateDate: 2019/8/1
+ */
+class MotionActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_scene_start)
+    }
+
+}
